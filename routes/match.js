d59ab274732f62f3eb63fbc036a0cdae64c8a19d
Adrian Conlin <adrian.conlin@gmail.com>
2013-08-27 21:18:40 +0100

    Refactored the match data processing out of the route.

--- routes/match.js
+++ routes/match.js
@@ -4,10 +4,8 @@
 var express = require('express');
 var JsTrueSkill = require('jstrueskill');
 var GameInfo = JsTrueSkill.GameInfo;
-var Player = JsTrueSkill.Player;
-var Rating = JsTrueSkill.Rating;
-var Team = JsTrueSkill.Team;
 var FactorGraphTrueSkillCalculator = JsTrueSkill.FactorGraphTrueSkillCalculator;
+var MatchData = require('../lib/match/MatchData');
 
 exports.entry = function(req, res){
 	var gameInfo = GameInfo.getDefaultGameInfo();
@@ -26,43 +24,26 @@ exports.entry = function(req, res){
 
 exports.calculate = function(req, res) {
 	var gameInfo = GameInfo.getDefaultGameInfo();
-
-	var defaultRating = gameInfo.getDefaultRating();
-
-	var teamOneMean = req.param('teamOneMean') || defaultRating.getMean();
-	var teamOneStd = req.param('teamOneStd') || defaultRating.getStandardDeviation();
-	var teamOneRank = req.param('teamOneRank') || 1;
-	var teamTwoMean = req.param('teamTwoMean') || defaultRating.getMean();
-	var teamTwoStd = req.param('teamTwoStd') || defaultRating.getStandardDeviation();
-	var teamTwoRank = req.param('teamTwoRank') || 2;
-
-	var teamOneRating = new Rating(teamOneMean, teamOneStd);
-	var teamTwoRating = new Rating(teamTwoMean, teamTwoStd);
-
-	var player1 = new Player('1');
-	var player2 = new Player('2');
-
-	var team1 = new Team('One', player1, teamOneRating);
-	var team2 = new Team('Two', player2, teamTwoRating);
-
-	var teams = Team.concat(team1, team2);
+	var matchData = new MatchData(req);
+	var teams = matchData.getTeams();
+	var ranks = matchData.getRanks();
 
 	var calculator = new FactorGraphTrueSkillCalculator();
 
-	var newRatings = calculator.calculateNewRatings(gameInfo, teams, [teamOneRank, teamTwoRank]);
-	var teamOneNewRating = newRatings[player1];
-	var teamTwoNewRating = newRatings[player2];
+	var newRatings = calculator.calculateNewRatings(gameInfo, teams, ranks);
+	var players = matchData.getPlayers();
+
+	var teamOneNewRating = newRatings[players[0]];
+	var teamTwoNewRating = newRatings[players[1]];
 
 	res.render('match', {
-		teamOneRating: teamOneRating,
-		teamTwoRating: teamTwoRating,
 		teamOneNewRating: teamOneNewRating,
 		teamTwoNewRating: teamTwoNewRating,
 		teamOneMean: teamOneNewRating.getMean(),
 		teamOneStd: teamOneNewRating.getStandardDeviation(),
-		teamOneRank: 1,
+		teamOneRank: ranks[0],
 		teamTwoMean: teamTwoNewRating.getMean(),
 		teamTwoStd: teamTwoNewRating.getStandardDeviation(),
-		teamTwoRank: 2
+		teamTwoRank: ranks[1]
 	});
 };
