a33c02ab5d3ec95d19bb6359d5f48553fa2fb8ca
Adrian Conlin <adrian.conlin@gmail.com>
2013-09-04 20:35:39 +0100

    Added more match data validation.

--- test/server/match/MatchDataTests.js
+++ test/server/match/MatchDataTests.js
@@ -1,18 +1,59 @@
 var MatchData = require('../../../lib/match/MatchData');
 
-exports.throwsWithInvalidData = function(test) {
+exports.throwsWithoutTeamData = function(test) {
 	test.throws(( function() {new MatchData(undefined);} ),
 		"Expected new MatchData(undefined) to throw");
 
-	var matchData = {};
-	test.throws(( function() {new MatchData(matchData);} ),
+	var matchJson = {};
+	test.throws(( function() {new MatchData(matchJson);} ),
 		"Expected new MatchData({}) to throw");
 
-	matchData = {
+	matchJson = {
 		teamOne: {}
 	};
-	test.throws(( function() {new MatchData(matchData);} ),
+	test.throws(( function() {new MatchData(matchJson);} ),
 		"Expected new MatchData to throw due to missing teamTwo");
+
+	test.done();
+};
+
+exports.throwsWithEmptyTeamData = function(test) {
+	var matchJson = {
+		teamOne: {},
+		teamTwo: {}
+	};
+	test.throws(( function() {new MatchData(matchJson);} ),
+		"Expected new MatchData to throw with empty team objects");
+
+	test.done();
+};
+
+exports.okWithValidData = function(test) {
+	var matchJson = {
+		teamOne: {
+			id: 1,
+			rating: {
+				mean: 25,
+				std: 25/3
+			},
+			rank: 1
+		},
+		teamTwo: {
+			id: 0,
+			rating: {
+				mean: 25,
+				std: 25/3
+			},
+			rank: 2
+		}
+	};
+	var matchData = new MatchData(matchJson);
+	test.ok(matchData !== undefined, "Expected valid MatchData");
+
+	var expected = 2;
+	test.equals(matchData.getTeams().length, expected, "Number of teams expected: " + expected);
+	test.equals(matchData.getRanks().length, expected, "Number of ranks expected: " + expected);
+	test.equals(matchData.getPlayers().length, expected, "Number of players expected: " + expected);
 
 	test.done();
 };
