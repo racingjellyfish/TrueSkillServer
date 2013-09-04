ddd1b93ea6cd057239e33812d7299035bd5fd0d7
Adrian Conlin <adrian.conlin@gmail.com>
2013-09-04 21:26:00 +0100

    Added extra match data validation and updated jstrueskill module to v0.0.1 to make use of the Player and Team equals methods.

--- test/server/match/MatchDataTests.js
+++ test/server/match/MatchDataTests.js
@@ -1,4 +1,29 @@
 var MatchData = require('../../../lib/match/MatchData');
+var JsTrueSkill = require('jstrueskill');
+var Player = JsTrueSkill.Player;
+var Rating = JsTrueSkill.Rating;
+var Team = JsTrueSkill.Team;
+
+var MATCH_JSON = {
+	teamOne: {
+		id: 1,
+		name: 'Player1',
+		rating: {
+			mean: 25,
+			std: 25/3
+		},
+		rank: 1
+	},
+	teamTwo: {
+		id: 0,
+		name: 'Player2',
+		rating: {
+			mean: 25,
+			std: 25/3
+		},
+		rank: 2
+	}
+};
 
 exports.throwsWithoutTeamData = function(test) {
 	test.throws(( function() {new MatchData(undefined);} ),
@@ -29,31 +54,50 @@ exports.throwsWithEmptyTeamData = function(test) {
 };
 
 exports.okWithValidData = function(test) {
-	var matchJson = {
-		teamOne: {
-			id: 1,
-			rating: {
-				mean: 25,
-				std: 25/3
-			},
-			rank: 1
-		},
-		teamTwo: {
-			id: 0,
-			rating: {
-				mean: 25,
-				std: 25/3
-			},
-			rank: 2
-		}
-	};
-	var matchData = new MatchData(matchJson);
+	var matchData = new MatchData(MATCH_JSON);
 	test.ok(matchData !== undefined, "Expected valid MatchData");
 
 	var expected = 2;
 	test.equals(matchData.getTeams().length, expected, "Number of teams expected: " + expected);
 	test.equals(matchData.getRanks().length, expected, "Number of ranks expected: " + expected);
 	test.equals(matchData.getPlayers().length, expected, "Number of players expected: " + expected);
+
+	test.done();
+};
+
+exports.teamRanksMatchJson = function(test) {
+	var matchData = new MatchData(MATCH_JSON);
+
+	var expected = 1;
+	test.equals(matchData.getRank(0), expected, "Expected team one rank of: " + expected);
+	expected = 2;
+	test.equals(matchData.getRank(1), expected, "Expected team two rank of: " + expected);
+
+	test.done();
+};
+
+exports.playersMatchJson = function(test) {
+	var matchData = new MatchData(MATCH_JSON);
+
+	var players = matchData.getPlayers();
+	var expected = new Player('Player1');
+	test.ok(players[0].equals(expected), "Expected player: " + expected);
+	expected = new Player('Player2');
+	test.ok(players[1].equals(expected), "Expected player: " + expected);
+
+	test.done();
+};
+
+exports.teamsMatchJson = function(test) {
+	var matchData = new MatchData(MATCH_JSON);
+
+	var player = new Player('Player1');
+	var rating = new Rating(25, 25/3);
+	var expected = new Team('One', player, rating);
+	test.ok(matchData.getTeam(0).equals(expected), "Expected team: " + expected);
+	player = new Player('Player2');
+	expected = new Team('Two', player, rating);
+	test.ok(matchData.getTeam(1).equals(expected), "Expected team: " + expected);
 
 	test.done();
 };
