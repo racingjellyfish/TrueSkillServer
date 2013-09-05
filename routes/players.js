2d76e404a2fc120a5e0312cf9b9c0514d09f808a
Adrian Conlin <adrian.conlin@gmail.com>
2013-09-05 17:33:12 +0100

    Added a data store factory to abstract out data related code to allow for easier testing.

--- routes/players.js
+++ routes/players.js
@@ -3,10 +3,13 @@
  *
  * TODO: retrieve player data store from a factory to allow for testing...
  */
+var DataStoreFactory = require('../lib/DataStoreFactory');
 var PlayerDataStore = require('../lib/match/PlayerDataStore');
 
 exports.list = function(req, res) {
-	var playerDataStore = new PlayerDataStore();
+	var dataStoreFactory = DataStoreFactory.INSTANCE;
+
+	var playerDataStore = dataStoreFactory.getPlayerDataStore();
 
 	res.send(playerDataStore.list());
 };
